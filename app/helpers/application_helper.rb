c74caa31fbaf88664c4ac1eedc684bd5ef43b8d9
Muhammad Umar <umar.imtiaz@devsinc.com>
2020-09-15 13:39:29 +0500

    User::devise integration

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+def user_avatar(user, size)
+    if user.avatar.attached?
+        user.avatar
+    else
+        image_url("default_profile_pic.png")
+    end
+end
+
+
+
+
 end
---
+++ db/migrate/20200914133136_remove_is_female_from_users.rb
@@ -0,0 +1,5 @@
+class RemoveIsFemaleFromUsers < ActiveRecord::Migration[6.0]
+  def change
+    remove_column :users, :is_female, :boolean
+  end
+end
